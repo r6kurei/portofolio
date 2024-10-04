a443188e85f9c9fc248ea480abad1413a84236a4
r6kurei <r6kurei@gmail.com>
2024-10-04 21:28:55 +0700

    six

---
+++ data/pages.ts
@@ -0,0 +1,22 @@
+export const pages = [
+  {
+    path: '/',
+    title: 'Home',
+    desc: 'Rikurei is one of the developers in Indonesia. My name is Rikurei, feel free to say hi.',
+  },
+  {
+    path: '/about',
+    title: 'About',
+    desc: 'My name is Rikurei, you can call me Rikurei. I was born in Purwakarta, Indonesia.',
+  },
+  {
+    path: '/works',
+    title: 'Works',
+    desc: 'List of works that I am involved in...',
+  },
+  {
+    path: '/contact',
+    title: 'Contact',
+    desc: 'Have a question? Drop us a note here and i will be in touch soon....',
+  },
+];
